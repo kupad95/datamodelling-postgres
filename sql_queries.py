84a64432288f05f5ce75bf060155a10807b52151
kupad95 <34950511+kupad95@users.noreply.github.com>
2020-04-05 22:01:16 +0100

    primary key for songplays, update conflict users

--- sql_queries.py
+++ sql_queries.py
@@ -8,7 +8,7 @@ time_table_drop = "DROP TABLE  IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays(songplay_id VARCHAR, start_time TIMESTAMP REFERENCES time (start_time), user_id INT REFERENCES users (user_id), level VARCHAR, song_id VARCHAR REFERENCES songs (song_id), \
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays(songplay_id SERIAL PRIMARY KEY, start_time TIMESTAMP REFERENCES time (start_time), user_id INT REFERENCES users (user_id), level VARCHAR, song_id VARCHAR REFERENCES songs (song_id), \
                             artist_id VARCHAR REFERENCES artists (artist_id), session_id INT, \
                             location VARCHAR, user_agent VARCHAR)
                             """)
@@ -30,7 +30,7 @@ time_table_create = ("""CREATE TABLE IF NOT EXISTS time(start_time TIMESTAMP PRI
 songplay_table_insert = ("""INSERT INTO songplays VALUES (DEFAULT, %s, %s, %s, %s, %s, %s, %s, %s )
 """)
 
-user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO NOTHING 
+user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO UPDATE SET level = EXCLUDED.level 
 """)
 
 song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s)           
